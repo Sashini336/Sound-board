2f63fb922c7926cd5a6423dfd1005956926bd9c2
Sashini336 <46651489+Sashini336@users.noreply.github.com>
2023-06-21 11:48:29 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+const sounds = ['dog','rain', 'sad','hum','sneeze',]
+
+sounds.forEach(sound => {
+    const btn = document.createElement('button')
+    btn.classList.add('btn')
+
+    btn.innerText = sound
+
+    btn.addEventListener('click', () => {
+        document.getElementById(sound).play()
+    })
+
+    document.getElementById('buttons').
+    appendChild(btn)
+})
